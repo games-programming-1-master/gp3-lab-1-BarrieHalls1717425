36e647ebf0e187c296dc86128c5e6f2dc07973b5
Bobby Law <robert.law@gcu.ac.uk>
2020-10-22 12:21:17 +0100

    Initial commit

---
+++ Lab1.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "Application.h"
+
+int main(int argc, char *argv[])
+{
+	Application::GetInstance()->Run();
+
+	return 0;
+}
+
---
+++ pch.h
@@ -0,0 +1,13 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <string>
+#include <chrono>
+
+#include <glew.h>
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+#endif //PCH_H
